c18d43e4a5af814386242ae0cd9ca9b535b03cf6
Pluglug <upward571103@gmail.com>
2024-12-03 03:16:58 +0900

    Add Sphinx documentation setup

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pie_menu_editor_docs'
+copyright = '2024, Pluglug'
+author = 'Pluglug'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,7 +1,15 @@
-.. Pie Menu Editor Documentation master file
+.. pie_menu_editor_docs documentation master file, created by
+   sphinx-quickstart on Tue Dec  3 03:15:22 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
 Welcome to Pie Menu Editor's documentation!
 ===========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
 
 Contents:
 ---------
